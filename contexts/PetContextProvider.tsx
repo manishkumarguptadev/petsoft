13f5eb68897240d2fc257e0204a03cb58c7d8720
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-10-28 20:56:42 +0530

    active pet styling

--- contexts/PetContextProvider.tsx
+++ contexts/PetContextProvider.tsx
@@ -10,6 +10,8 @@ type PetContextProviderProps = {
 
 type TPetContext = {
   pets: Pet[];
+  selectedPetId: Pet["id"] | null;
+  handleChangeSelectedPetId: (id: Pet["id"]) => void;
 };
 
 export const PetContext = createContext<TPetContext | null>(null);
@@ -20,11 +22,17 @@ export default function PetContextProvider({
 }: PetContextProviderProps) {
   // state
   const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+  const handleChangeSelectedPetId = (id: Pet["id"]) => {
+    setSelectedPetId(id);
+  };
 
   return (
     <PetContext.Provider
       value={{
         pets,
+        selectedPetId,
+        handleChangeSelectedPetId,
       }}
     >
       {children}
